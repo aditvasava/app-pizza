ab90fec59e0fe0c6983d1a17f265a6e19acee4c2
Adit Vasava <asv2000p@gmail.com>
2024-03-22 22:28:43 -0400

    Updates

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
   const navigate = useNavigate();
@@ -13,4 +13,3 @@ function NotFound() {
 }
 
 export default NotFound;
-~
